a2f8e488f2483af736d023c53628b13ed4d7d373
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-04-06 20:32:32 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,6 +5,7 @@ def find_min_in_nested_arrays(src)
   row_i = 0 
   while row_i < src.length do 
     min = src[row_i][0]
+    puts min
     col_i = 1 
     while col_i < src[row_i][col_i] do 
       if src[row_i][col_i] < min 
